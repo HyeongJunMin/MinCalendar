952e9f4240d7d66ea300bfa6286bc14795c19a9f
HyeongJunMin <alsgudwns007@naver.com>
2019-10-01 12:32:39 +0900

    + heroku lombok test

--- src/main/java/com/example/demo/calendar/model/CalReqDTO.java
+++ src/main/java/com/example/demo/calendar/model/CalReqDTO.java
@@ -2,19 +2,10 @@ package com.example.demo.calendar.model;
 
 import java.io.Serializable;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 /**달력값을 요청할 때 연,월,일 정보를 전달하기 위한 용도
  * @author minhj
  *
  */
-@Getter
-@Setter
-@ToString
-@Builder
 public class CalReqDTO implements Serializable {
 
 	private int reqYear;
@@ -28,4 +19,30 @@ public class CalReqDTO implements Serializable {
 		this.reqMonth = reqMonth;
 		this.reqDate = reqDate;
 	}
+	
+	public int getReqYear() {
+		return reqYear;
+	}
+
+	public void setReqYear(int reqYear) {
+		this.reqYear = reqYear;
+	}
+
+	public int getReqMonth() {
+		return reqMonth;
+	}
+
+	public void setReqMonth(int reqMonth) {
+		this.reqMonth = reqMonth;
+	}
+
+	public int getReqDate() {
+		return reqDate;
+	}
+
+	public void setReqDate(int reqDate) {
+		this.reqDate = reqDate;
+	}
+
+	
 }
